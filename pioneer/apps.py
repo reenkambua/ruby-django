8ff97efef5168167d2b894474f01e156c81a3443
reenkambua <maureenkambua75@gmail.com>
2024-11-06 09:24:49 +0300

    first commit

---
+++ pioneer/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PioneerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pioneer'
---
+++ pioneer/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,  "index.html")
+def about(request):
+    return render(request, "about.html")
+def contact(request):
+    return render(request, "contacts.html")
+def services(request):
+    return render(request, "services.html")
+def blog(request):
+    return render(request, "blogs.html")
